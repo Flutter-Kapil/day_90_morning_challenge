3e8446cd7fdc59356bafd953a7f547e5b7d0f7ce
Kapil Jhajhria <jhajhria44@gmail.com>
2020-02-28 10:55:13 +0530

    completed without formula as well

--- bin/main.dart
+++ bin/main.dart
@@ -7,9 +7,20 @@
 // triangle(215) ➞ 23220
 
 void main() {
+  print(triangle(1));
+  print(triangle(3));
+  print(triangle(6));
   print(triangle(215));
 }
 
-int triangle(int n){
+int triangle2(int n){
   return ((n*(n+1))~/2);
+}
+
+int triangle(int n){
+  var count=0;
+  for(int i=0;i<=n;i++){
+    count=count+i;
+  }
+  return count;
 }
